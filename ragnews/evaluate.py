30c0c0014bd45b83fd6bfd0b1a4a3f51b6cfafcd
finnless <nolan@nolanwindham.com>
2024-09-21 15:38:54 -0700

    progress

--- ragnews/evaluate.py
+++ ragnews/evaluate.py
@@ -22,6 +22,24 @@ class RAGClassifier:
         '''
         pass
 
+    def _extract_cloze_keywords(text, seed=0, temperature=None):
+        r'''
+        This is a helper function for RAG with the Cloze test.
+        Given an input text,
+        this function extracts the keywords that will be used to perform the search for articles that will be used in RAG.
+
+        >>> text1 = """On July 13, 2024, during a campaign rally in Butler, Pennsylvania, presidential candidate [MASK0] was shot at in a failed assassination attempt. The gunfire caused minor damage to [MASK0]'s upper right ear, while one spectator was killed and two others were critically injured. On September 15, 2024, the security detail of [MASK0] spotted an armed man while the former president was touring his golf course in West Palm Beach. They opened fire on the suspect, which fled on a vehicle and was later captured thanks to the contribution of an eyewitness. In the location where the suspect was spotted, the police retrieved an AK-47-style rifle with a scope, two rucksacks and a GoPro."""
+        >>> RAGClassifier._extract_cloze_keywords(text1)
+        'campaign, shooting, Pennsylvania, presidential, assassination'
+        >>> text2 = """After a survey by the Associated Press of Democratic delegates on July 22, 2024, [MASK0] became the new presumptive candidate for the Democratic party, a day after declaring her candidacy. She would become the official nominee on August 5 following a virtual roll call of delegates."""
+        >>> RAGClassifier._extract_cloze_keywords(text2)
+        '"2024 democratic presumptive candidate"'
+        '''
+        # TODO put OR between simmilar keywords
+        # TODO use the context from the predicate of the relevant sentence to create keywords most likely to return articles that mention the subject
+        system = 'You are helping a team do Cloze test. Your task is to write a search query that will retrieve relevant articles from a news database. Craft your query with only the most important keywords related to the masked text. Limit to 5 keywords at most. Return only search keywords, do not include any other text or numbers.'
+        return ragnews.run_llm(system, text, seed=seed, temperature=temperature)
+
     def predict(self, masked_text: str, attempt=0):
         '''
         Predict the labels of the documents.
@@ -32,19 +50,22 @@ class RAGClassifier:
         ['Harris']
         >>> model.predict('[MASK0] is the democratic nominee and [MASK1] is the republican nominee')
         ['Harris', 'Trump']
+        >>> text1 = """On July 13, 2024, during a campaign rally in Butler, Pennsylvania, presidential candidate [MASK0] was shot at in a failed assassination attempt. The gunfire caused minor damage to [MASK0]'s upper right ear, while one spectator was killed and two others were critically injured. On September 15, 2024, the security detail of [MASK0] spotted an armed man while the former president was touring his golf course in West Palm Beach. They opened fire on the suspect, which fled on a vehicle and was later captured thanks to the contribution of an eyewitness. In the location where the suspect was spotted, the police retrieved an AK-47-style rifle with a scope, two rucksacks and a GoPro."""
+        >>> model.predict(text1)
+        ['Trump']
+        >>> text2 = """After a survey by the Associated Press of Democratic delegates on July 22, 2024, [MASK0] became the new presumptive candidate for the Democratic party, a day after declaring her candidacy. She would become the official nominee on August 5 following a virtual roll call of delegates."""
+        >>> model.predict(text2)
+        ['Harris']
         '''
+        db = ragnews.ArticleDB('ragnews.db')
+        
         if not re.search(r'\[MASK\d+\]', masked_text):
             return []
-        db = ragnews.ArticleDB('ragnews.db')
-        rag_query = re.sub(r'\[MASK\d+\]', '', masked_text).strip()
-        # TODO fix this up
         masks = list(set(re.findall(r'\[MASK\d+\]', masked_text)))
-        rag_query = rag_query + '\n\nI am doing a Cloze test on the above text. Fill in the blanks. Who are: ' + ', '.join(masks)
-        rag_result = ragnews.rag(rag_query, db)
         system = (
             'You are a helpful assistant that predicts the answers of the masked text '
             'based only on the context provided. '
-            'Masked text is in the format of [MASK0], [MASK1], etc. '
+            'Masked text is in the format of {masks}.'
             'Think through your answer step-by-step in no more than 50 words. '
             'If your answer is a person, provide their last name ONLY. '
             'Once you have a final answer for each mask, provide each answer on a new line at the end of your response after a a divider line (---).\n'
@@ -55,17 +76,9 @@ class RAGClassifier:
             'Alice\n'
             'Bob'
         )
-        user = (
-            '# Context\n'
-            '{context}\n\n'
-            '# Masked Text\n'
-            '{masked_text}'
-        )
-        user = user.format(context=rag_result, masked_text=masked_text)
-        logging.debug('user:\n%s', user)
-        output = ragnews.run_llm(system, user)
-        logging.debug('output:\n%s', output)
-
+        system = system.format(masks=' '.join(masks))
+        keywords = RAGClassifier._extract_cloze_keywords(masked_text)
+        output = ragnews.rag(masked_text, db, keywords=keywords, system=system)
         # if the output is not in the correct format, try again
         if '---' not in output and attempt < 3:
             logging.warning('error parsing output, trying again... attempt: %d', attempt)
